85f35e31dbc1b122cf2e9ef6ff0864974bbd1366
Júlio Ricardo Petters Siqueira <saintbr@gmail.com>
2026-02-03 16:45:24 +0000

    Expand docker compose to all language services

--- kpax/zig/src/main.zig
+++ kpax/zig/src/main.zig
@@ -1,27 +1,25 @@
 const std = @import("std");
-const zig = @import("zig");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try zig.bufferedPrint();
-}
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    var server = std.http.Server.init(allocator, .{ .reuse_address = true });
+    defer server.deinit();
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    try server.listen(.{ .port = 8080, .address = .{ .any = {} } });
+    std.log.info("Server running on http://0.0.0.0:8080", .{});
+
+    while (true) {
+        var res = try server.accept(.{ .allocator = allocator });
+        defer res.deinit();
+
+        try res.respond("{\"message\":\"hello world\"}", .{
+            .status = .ok,
+            .extra_headers = &.{
+                .{ .name = "content-type", .value = "application/json" },
+            },
+        });
+    }
 }
